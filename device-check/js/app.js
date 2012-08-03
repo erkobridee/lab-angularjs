825ab911ccdee8f0562ee90d9a97168390537040
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2012-08-02 21:35:58 -0300

    ajuste da definicao do app.js

--- device-check/js/app.js
+++ device-check/js/app.js
@@ -6,27 +6,3 @@ angular.module('App', [])
       .when('/tablet', {controller: ctrl.TabletCtrl, templateUrl:'tpl/tablet.html'})
       .when('/desktop', {controller: ctrl.DesktopCtrl, templateUrl:'tpl/desktop.html'})
   });
-
-/*
-function CheckDeviceCtrl($scope, $location) {
-
-  console.log(navigator);
-
-  console.log( $.device )
-
-  $location.path(['', $.device.type, ''].join('/'));
-
-}
-
-function MobileCtrl($scope) {
-  $scope.pageName = 'Mobile Page';
-}
-
-function TabletCtrl($scope) {
-  $scope.pageName = 'Tablet Page';
-}
-
-function DesktopCtrl($scope) {
-  $scope.pageName = 'Desktop Page';
-}
-*/
