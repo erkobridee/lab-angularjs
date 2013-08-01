a0483c97eb82c33094dc354ede638ffd2e5e9c91
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-07-31 23:06:01 -0300

    jshint - js code quality reviewed 100% ok now

--- lab/device-check/js/controller.js
+++ lab/device-check/js/controller.js
@@ -6,23 +6,23 @@ var Ctrl = (function() {
 
     console.log(navigator);
 
-    console.log( $.device )
+    console.log( $.device );
 
     $location.path(['', $.device.type, ''].join('/'));
 
-  }
+  };
 
   Ctrl.prototype.Mobile = function($scope) {
     $scope.pageName = 'Mobile Page';
-  }
+  };
 
   Ctrl.prototype.Tablet = function($scope) {
     $scope.pageName = 'Tablet Page';
-  }
+  };
 
   Ctrl.prototype.Desktop = function($scope) {
     $scope.pageName = 'Desktop Page';
-  }
+  };
 
   return Ctrl;
 
