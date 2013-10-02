537454cd02b077c8101d7e78adc13610eaa2bba8
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-10-01 22:37:20 -0300

    exemplo de rotas com requirejs + angularjs

---
+++ lab/route/02/scripts/routes.js
@@ -0,0 +1,23 @@
+angular.module('app').config(
+
+  // dependencies injection
+  ['$routeProvider',
+
+function($routeProvider) {
+
+    $routeProvider
+      .when(
+        '/', 
+        {
+          controller: 'HomeCtrl',
+          templateUrl:'views/home.html'}
+      )
+      .when(
+        '/about', 
+        {
+          controller: 'AboutCtrl',
+          templateUrl:'views/about.html'
+        }
+      );
+
+}]);
