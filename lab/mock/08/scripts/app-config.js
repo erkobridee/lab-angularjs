77610ba35325bbf88aaa04e646f3f160c8307286
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-10-05 00:17:06 -0300

    angularjs mock httpBackend 08 - requirejs config updated : mock defined by boolean flag

--- lab/mock/08/scripts/app-config.js
+++ lab/mock/08/scripts/app-config.js
@@ -3,52 +3,31 @@
 
   //---
 
-  require({
+  var MOCK_FLAG = true;
 
-    // libraries dependencies with fallback
+  var ANGULAR_VERSION = '1.0.7';
+
+  //---
+
+  // require object config
+  var config = {
+
+    // libraries dependencies with fallback 
     paths: {
 
       'angular': [ 
-        '//ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min',
-        '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.0.7/angular.min',
-        'http://code.angularjs.org/1.0.7/angular.min'      
-      ],
-
-      'angular-mocks': [
-        'http://code.angularjs.org/1.0.7/angular-mocks'
-      ],
-
-      'angular-mocks-backend': [
-        'mock/angular-mocks-backend'
-      ],
-
-      'app-main': [
-        'app-mock'
+        '//ajax.googleapis.com/ajax/libs/angularjs/'+ANGULAR_VERSION+'/angular.min',
+        '//cdnjs.cloudflare.com/ajax/libs/angular.js/'+ANGULAR_VERSION+'/angular.min',
+        'http://code.angularjs.org/'+ANGULAR_VERSION+'/angular.min'      
       ]
 
     },
 
-    // define js scripts dependencies
+    // define js scripts dependencies 
     shim: {
 
-      'angular-mocks': {
-        deps: ['angular']
-      },
-
-      'angular-mocks-backend': {
-        deps: ['angular', 'angular-mocks']
-      },
-
-      'mock/resources/allow-jsonp-pass-external': {
-        deps: ['angular-mocks-backend']
-      },
-
-      'mock/resources/UserMock': {
-        deps: ['angular-mocks-backend']
-      },
-
       'app-main': { 
-        deps: ['angular', 'angular-mocks-backend'] 
+        deps: ['angular'] 
       },
 
       'controllers/UserCtrl': { 
@@ -57,15 +36,64 @@
 
       'app-start': { 
         deps: [
-          'mock/resources/allow-jsonp-pass-external',
-          'mock/resources/UserMock',
           'controllers/UserCtrl'
         ] 
       }
 
     }
 
-  },
+  };
+  
+  //---
+
+  if(MOCK_FLAG) {
+
+    //------------------
+    // add more libraries dependencies
+
+    config.paths['angular-mocks'] = [
+      'http://code.angularjs.org/'+ANGULAR_VERSION+'/angular-mocks'
+    ];
+
+    config.paths['angular-mocks-backend'] = [
+      'mock/angular-mocks-backend'
+    ];
+
+    config.paths['app-main'] = [
+      'app-mock'
+    ];
+
+    //------------------
+    // add more shim configs
+
+    config.shim['angular-mocks'] = {
+      deps: ['angular']
+    };
+
+    config.shim['angular-mocks-backend'] = {
+      deps: ['angular', 'angular-mocks']
+    };
+
+    config.shim['mock/resources/allow-jsonp-pass-external'] = {
+      deps: ['angular-mocks-backend']
+    };
+
+    config.shim['mock/resources/UserMock'] = {
+      deps: ['angular-mocks-backend']
+    };
+
+    config.shim['app-main'].deps.push('angular-mocks-backend');
+
+    config.shim['app-start'].deps.push('mock/resources/allow-jsonp-pass-external');
+    config.shim['app-start'].deps.push('mock/resources/UserMock');
+
+  }
+
+  console.log(config);
+
+  //---
+
+  require(config,
 
     ['require'],
 
