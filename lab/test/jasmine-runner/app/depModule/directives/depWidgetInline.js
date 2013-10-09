0af881b3cf5d6053f81f70af9634e6554aa8b081
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-10-09 11:18:08 -0300

    angularjs jasmine test - depModule widget directive : inline and outline template

---
+++ lab/test/jasmine-runner/app/depModule/directives/depWidgetInline.js
@@ -0,0 +1,26 @@
+angular.module('app.dep').directive(
+
+  // directive name
+  'depWidgetInline', 
+
+// directive definition
+function() {
+
+  return {
+
+    restrict: 'EA', 
+      // E - Element <dep-widget-inline title="my widget">widget content</dep-widget-inline> 
+      // A - Attribute <div data-dep-widget-inline title="my widget">widget content</div>
+      // C - Class <div class="dep-widget-inline" title="my widget">widget content</div>
+    replace: true,
+    transclude: true,
+    
+    template: '<div class="widget"><div class="content" ng-transclude></div></div>',
+
+    link: function(scope, element, attrs) {
+      element.prepend("<div class='title'>" + attrs.title + "</div>");
+    }
+
+  };
+
+});
