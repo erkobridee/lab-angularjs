5e7a0e8ba9e7dd577780608ecf57da68e6058b20
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-10-10 23:52:30 -0300

    angularjs mock - angular-mocks-backend.js updated to v0.1.1
    https://github.com/erkobridee/angular-mocks-backend/releases/tag/v0.1.1

--- lab/mock/07/scripts/mock/angular-mocks-backend.js
+++ lab/mock/07/scripts/mock/angular-mocks-backend.js
@@ -1,5 +1,5 @@
 (function(angular) {
-  "use strict";
+  'use strict';
 
 
   //--- Custom Error
@@ -51,9 +51,15 @@
     };
 
     ClassDef.prototype.config = function(angular, httpBackend) {
+      configAllow(httpBackend);
       configResources(angular, httpBackend);
     };
 
+
+    var configAllow = function(httpBackend) {      
+      // Allow get html to load templates
+      httpBackend.when('GET', regexpUrl(/.html$/)).passThrough();
+    };
 
     //--- mocked resources
 
