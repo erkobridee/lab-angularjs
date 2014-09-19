03e18332f721e82798ffc37d7f1581f5e1767ac6
Erko Bridee <erko.bridee@gmail.com>
2014-09-18 22:58:03 -0300

    requirejs commonjs little update
    app/main/module.js

--- src/lab/requirejs-commonjs-style/app/main/module.js
+++ src/lab/requirejs-commonjs-style/app/main/module.js
@@ -4,10 +4,10 @@ define(function(require) {
   var angular = require('angular');
 
   // require('../moduleA/require.load');
-  require('../home/require.load');
-  require('../about/require.load');
-  require('../github/require.load');
-  require('../pageWithoutController/require.load');
+  // require('../home/require.load');
+  // require('../about/require.load');
+  // require('../github/require.load');
+  // require('../pageWithoutController/require.load');
 
   // angular module definition
   return angular.module(
@@ -16,11 +16,16 @@ define(function(require) {
 
     // module dependencies
     [
-      'moduleA',
-      'home',
-      'about',
-      'github',
-      'pageWithoutController'
+      // 'moduleA',
+      // 'home',
+      // 'about',
+      // 'github',
+      // 'pageWithoutController'
+      require('../moduleA/require.load').name,
+      require('../home/require.load').name,
+      require('../about/require.load').name,
+      require('../github/require.load').name,
+      require('../pageWithoutController/require.load').name
     ]
 
   );
