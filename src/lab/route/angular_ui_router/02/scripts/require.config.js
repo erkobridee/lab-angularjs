400187c5c01efe6d38a5a07e137201d4f1e2d497
Erko Bridee <erko.bridee@gmail.com>
2014-11-10 16:44:32 -0200

    angular ui router example 02 defined

---
+++ src/lab/route/angular_ui_router/02/scripts/require.config.js
@@ -0,0 +1,80 @@
+require({
+
+  // libraries dependencies with fallback
+  paths: {
+
+    jquery: [
+      '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
+      '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
+      '/cdn/ajax/libs/jquery/2.1.1/jquery.min'
+    ],
+
+    // http://code.angularjs.org/1.3.1/
+    angular: [
+      '/cdn/ajax/libs/angular.js/1.3.1/angular.min'
+    ],
+
+    angular_resource: [
+      '/cdn/ajax/libs/angular.js/1.3.1/angular-resource.min'
+    ],
+
+    angular_ui_router: [
+      '/cdn/ajax/libs/angular-ui/ui-router/0.2.11/angular-ui-router.min'
+    ]
+
+  },
+
+  // define js scripts dependencies
+  shim: {
+
+    'angular': {
+      deps: ['jquery']
+    },
+
+    'angular_resource': {
+      deps: ['angular']
+    },
+
+    'angular_ui_router': {
+      deps: ['angular']
+    },
+
+    'main/module': {
+      deps: [
+        'angular_ui_router',
+        'angular_resource'
+      ]
+    },
+
+    'controllers/home': {
+      deps: ['main/module']
+    },
+
+    'controllers/about': {
+      deps: ['main/module']
+    },
+
+    'main/routes': {
+      deps: [
+        'controllers/home',
+        'controllers/about'
+      ]
+    },
+
+    'main/start': {
+      deps: [
+        'main/routes'
+      ]
+    }
+
+  }
+
+},
+
+['require'], function(require) {
+
+  console.log('calling main/start.js');
+
+  require(['main/start']);
+
+});
