b2b05d87bb861fc6d0619e893bbea86e55afd0d0
Erko Bridee <erko.bridee@gmail.com>
2014-11-20 11:49:52 -0200

    require.unit.load.js updated

--- src/lab/test/jasmine-runner/04/src/require.unit.load.js
+++ src/lab/test/jasmine-runner/04/src/require.unit.load.js
@@ -2,7 +2,7 @@ define(function(require) {
   'use strict';
 
   require('./app/main/tests/unit/package');
-  // require('./app/home/tests/unit/package');
+  require('./app/home/tests/unit/package');
   require('./app/about/tests/unit/package');
   // require('./app/dep1/tests/unit/package');
 
