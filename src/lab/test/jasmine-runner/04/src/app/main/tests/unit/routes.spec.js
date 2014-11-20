20244408e20b903a1ad5ffe83b79817c9b49362d
Erko Bridee <erko.bridee@gmail.com>
2014-11-20 10:50:57 -0200

    exemplo de teste de rota com ui router atualizado

--- src/lab/test/jasmine-runner/04/src/app/main/tests/unit/routes.spec.js
+++ src/lab/test/jasmine-runner/04/src/app/main/tests/unit/routes.spec.js
@@ -4,51 +4,41 @@ describe('Angular.js \'main\' Routes', function() {
 
   beforeEach(function() {
 
+    // necessary? probably no...
     module('main', function($stateProvider, $urlRouterProvider) {
       stateProvider = $stateProvider;
       urlRouterProvider = $urlRouterProvider;
     });
-    // module('ui.router');
 
     inject(function($state) {
       state = $state;
-
-      // console.log(state);
     });
 
-    // console.log('main/routes.spec.js');
-    // console.log(state.states);
-
   });
-
-  /* only to check if injection work fine
-  it('$route should be defined', function() {
-    expect(route).not.toEqual(null);
-  });
-  */
-
-  // http://bardo.io/posts/testing-your-ui-router-configuration/
 
   describe("States Map", function() {
 
     it('$state should be present', function() {
 
-      var config = state.get('404');
+      // console.log(stateProvider);
+      // console.log(urlRouterProvider);
 
-      console.log(stateProvider);
-      console.log(urlRouterProvider);
-      console.log(config);
+      // var config = state.get('404');
+      // console.log(config);
 
-      // expect(state).not.toEqual(null);
       expect(state).not.toBeNull();
     });
 
     describe("404 state", function() {
 
-      var config;
+      var config, views;
 
-      beforeEach(function() {
+      it("should be defined", function() {
+        // arrange
         config = state.get('404');
+
+        // assertions
+        expect(config).toBeDefined();
       });
 
       it("should map url to \'/404\'", function() {
@@ -56,47 +46,19 @@ describe('Angular.js \'main\' Routes', function() {
       });
 
       it("should views be defined", function() {
-        expect(config.views).toBeDefined();
+        // arrange
+        views = config.views;
+
+        // assertions
+        expect(views).toBeDefined();
       });
 
       it("should map views.master templateUrl to \''app/main/templates/404.html'\'", function() {
-        expect(config.views.master.templateUrl).toEqual('app/main/templates/404.html');
+        expect(views.master.templateUrl).toEqual('app/main/templates/404.html');
       });
 
     });
 
-  });
-
-  /*
-  describe('Routes Map', function() {
-
-    / * only to check if injection work fine
-    it('$route should be present', function() {
-      expect(route).not.toEqual(null);
-      expect(route).not.toBeNull();
-    });
-    * /
-
-    describe('location \'/404\'', function() {
-
-      it('should be defined', function() {
-        expect(route.routes['/404']).toBeDefined();
-      });
-
-      it('should map to templateUrl app/main/templates/404.html', function() {
-        expect(route.routes['/404'].templateUrl).toEqual('app/main/templates/404.html');
-      });
-
-    });
-
-    describe('otherwise is the empty string', function() {
-
-      it('should redirect to location \'/404\'', function() {
-        expect(route.routes[null].redirectTo).toEqual('/404');
-      });
-
-    });
-
-  }); */ //--- end: Routes Map
+  }); //--- end: States Map
 
 });
