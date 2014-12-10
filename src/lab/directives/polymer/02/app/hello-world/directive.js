6b6d49fbf41e193dd0634e4f477c68543ee59d0d
Eduardo Royer <dudu_royer@hotmail.com>
2014-12-10 19:38:41 -0200

    Added Polymer : 02 : Integration with AngularJS

---
+++ src/lab/directives/polymer/02/app/hello-world/directive.js
@@ -0,0 +1,45 @@
+(function() {
+  'use strict';
+
+  angular.module( 'app' ).directive( 'helloWorld', helloWorld );
+
+  //---
+
+  function helloWorld() {
+
+    var scope = {
+      name: '='
+    };
+
+    var directive = {
+      restrict: 'EA',
+      scope: scope,
+      link: linkFunc,
+      templateUrl: 'app/hello-world/template.html'
+    };
+
+    return directive;
+
+    //---
+
+    function linkFunc( scope, element, attrs ) {
+
+      init();
+
+      //---
+
+      scope.$watch( 'name', function( value ) {
+        element[0].name = value;
+      });
+
+      //---
+
+      function init() {
+        Polymer( 'hello-world', {} );
+      }
+
+    }
+
+  }
+
+})();
