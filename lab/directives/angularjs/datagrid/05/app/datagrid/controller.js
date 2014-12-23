8efd2cdaeb9783ddcb13bee09f69add3303140d7
Erko Bridee <erko.bridee@gmail.com>
2014-12-23 11:46:22 -0200

    gh-pages automated commit 2014-12-23T13:46:21Z (UTC)

---
+++ lab/directives/angularjs/datagrid/05/app/datagrid/controller.js
@@ -0,0 +1,57 @@
+(function() {
+  'use strict';
+
+  angular.module('app').controller('DatagridCtrl', DatagridCtrl);
+
+  //---
+
+  DatagridCtrl.$inject = [ '$scope' ];
+
+  function DatagridCtrl($scope) {
+    var vm = this;
+
+    //---
+
+    $scope.data = $scope.data || [];
+    $scope.selectedValues = $scope.selectedValues || [];
+
+    vm.select = select;
+    vm.selectAll = selectAll;
+
+    //---
+
+    function selectAll( value ) {
+      $scope.$emit('datatable:selectAll', value);
+
+      if (value === false) {
+        $scope.selectedValues = [];
+        return;
+      }
+
+      for(var i = 0; i < $scope.data.length; i++) {
+        var contains = ~$scope.selectedValues.indexOf($scope.data[i]);
+        if(!contains) {
+          $scope.selectedValues.push($scope.data[i]);
+        }
+      }
+    }
+
+    function select( row, value ) {
+      var $index = $scope.data.indexOf(row);
+      var selectedIndex = $scope.selectedValues.indexOf(row);
+
+      if (value === true && selectedIndex < 0) {
+        $scope.selectedValues.push(row);
+      } else if (value === false && selectedIndex >= 0) {
+        $scope.selectedValues.splice(selectedIndex, 1);
+      }
+
+      row.isSelected = value;
+
+      var allSelected = ($scope.selectedValues.length === $scope.data.length);
+      $scope.$emit('datatable:allSelected', allSelected);
+    }
+
+  }
+
+})();
