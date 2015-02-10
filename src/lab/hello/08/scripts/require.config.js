19fa6d6adee213bfbfabb933a0c63782f2e9d016
Erko Bridee <erko.bridee@gmail.com>
2015-02-10 17:52:17 -0200

    hello/08 lib's ref updated

--- src/lab/hello/08/scripts/require.config.js
+++ src/lab/hello/08/scripts/require.config.js
@@ -4,8 +4,8 @@ require({
   paths: {
 
     angular: [
-      //'http://code.angularjs.org/1.3.4/angular.min',
-      '/cdn/ajax/libs/angular.js/1.3.4/angular.min'
+      //'http://code.angularjs.org/1.3.13/angular.min',
+      '/cdn/ajax/libs/angular.js/1.3.13/angular.min'
     ]
 
   },
