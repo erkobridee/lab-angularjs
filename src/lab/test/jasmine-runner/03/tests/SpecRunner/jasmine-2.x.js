b10ca5c2b130c993ee8aad15d03afa62c4ebc99c
Erko Bridee <erko.bridee@gmail.com>
2015-05-28 18:23:37 -0300

    test/jasmine-runner/03 updated
    require.js v2.1.17
    angular.js v1.4.0
    added jasmine Spec Runner 2.3.4

--- src/lab/test/jasmine-runner/03/tests/SpecRunner/jasmine-2.x.js
+++ src/lab/test/jasmine-runner/03/tests/SpecRunner/jasmine-2.x.js
@@ -13,20 +13,20 @@
       ],
 
       angular: [
-        '/cdn/ajax/libs/angular.js/1.3.13/angular.min'
+        '/cdn/ajax/libs/angular.js/1.4.0/angular.min'
       ],
 
       angularRoute: [
-        '/cdn/ajax/libs/angular.js/1.3.13/angular-route.min'
+        '/cdn/ajax/libs/angular.js/1.4.0/angular-route.min'
       ],
 
       angularResource: [
-        '/cdn/ajax/libs/angular.js/1.3.13/angular-resource.min'
+        '/cdn/ajax/libs/angular.js/1.4.0/angular-resource.min'
       ],
 
 
       angularMocks: [
-        '/cdn/ajax/libs/angular.js/1.3.13/angular-mocks'
+        '/cdn/ajax/libs/angular.js/1.4.0/angular-mocks'
       ]
 
     },
