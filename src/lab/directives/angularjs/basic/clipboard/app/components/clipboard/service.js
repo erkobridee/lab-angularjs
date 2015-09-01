dd8767fc4c955ebeebb976a2ffad638c4d519877
Erko Bridee <erko.bridee@gmail.com>
2015-09-01 12:50:39 -0300

    refactor(clipboard directive): review service

--- src/lab/directives/angularjs/basic/clipboard/app/components/clipboard/service.js
+++ src/lab/directives/angularjs/basic/clipboard/app/components/clipboard/service.js
@@ -35,23 +35,30 @@
       var navigator = $window.navigator,
           ua = navigator.userAgent, tem,
           M = ua.match( /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i ) || [];
-      if( /trident/i.test( M[1] ) ) {
-        tem =  /\brv[ :]+(\d+)/g.exec(ua) || [];
-        return 'IE ' + ( tem[1] || '' );
-      }
-      if( M[1] === 'Chrome' ){
-        tem = ua.match( /\bOPR\/(\d+)/ );
-        if( tem !== null ) {
-          return 'Opera ' + tem[1];
+
+      var output = (function() {
+        if( /trident/i.test( M[1] ) ) {
+          tem =  /\brv[ :]+(\d+)/g.exec(ua) || [];
+          return 'IE ' + ( tem[1] || '' );
         }
-      }
-      M = M[2] ? [M[1], M[2]] : [ navigator.appName, navigator.appVersion, '-?' ];
-      if( ( tem = ua.match( /version\/(\d+)/i ) ) !== null ) {
-        M.splice( 1, 1, tem[1] );
-      }
+        if( M[1] === 'Chrome' ){
+          tem = ua.match( /\bOPR\/(\d+)/ );
+          if( tem !== null ) {
+            return 'Opera ' + tem[1];
+          }
+        }
+        M = M[2] ? [M[1], M[2]] : [ navigator.appName, navigator.appVersion, '-?' ];
+        if( ( tem = ua.match( /version\/(\d+)/i ) ) !== null ) {
+          M.splice( 1, 1, tem[1] );
+        }
+        return M.join(' ');
+      })().split(' ');
+
+      output[0] = output[0].toLowerCase();
+      output[1] = output[1].trim();
       return {
-        name: M[0].toLowerCase(),
-        version: +M[1]
+        name: output[0],
+        version: ( output[1] ? +output[1] : -1 )
       };
     })();
 
