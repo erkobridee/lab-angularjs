8fa0f4326dee2554e48af5fdce59d4b15d776965
Erko Bridee <erko.bridee@gmail.com>
2016-06-07 22:30:48 -0300

    cep example - update angular to 1.5.6

--- src/lab/consulta_cep/require.config.js
+++ src/lab/consulta_cep/require.config.js
@@ -4,15 +4,15 @@ require({
   paths: {
 
     angular: [
-      '/cdn/ajax/libs/angular.js/1.4.1/angular.min'
+      '/cdn/ajax/libs/angular.js/1.5.6/angular.min'
     ],
 
     angularRoute: [
-      '/cdn/ajax/libs/angular.js/1.4.1/angular-route.min'
+      '/cdn/ajax/libs/angular.js/1.5.6/angular-route.min'
     ],
 
     angularResource: [
-      '/cdn/ajax/libs/angular.js/1.4.1/angular-resource.min'
+      '/cdn/ajax/libs/angular.js/1.5.6/angular-resource.min'
     ]
 
   },
