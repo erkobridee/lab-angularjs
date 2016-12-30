ab57c19d3401458fc2e9613c75c7b17228355010
Erko Bridee <erko.bridee@gmail.com>
2016-12-30 21:31:38 -0200

    update consulta cep example to use angular.js v1.6.1

--- src/lab/consulta_cep/require.config.js
+++ src/lab/consulta_cep/require.config.js
@@ -4,15 +4,15 @@ require({
   paths: {
 
     angular: [
-      '/cdn/ajax/libs/angular.js/1.5.6/angular.min'
+      '/cdn/ajax/libs/angular.js/1.6.1/angular.min'
     ],
 
     angularRoute: [
-      '/cdn/ajax/libs/angular.js/1.5.6/angular-route.min'
+      '/cdn/ajax/libs/angular.js/1.6.1/angular-route.min'
     ],
 
     angularResource: [
-      '/cdn/ajax/libs/angular.js/1.5.6/angular-resource.min'
+      '/cdn/ajax/libs/angular.js/1.6.1/angular-resource.min'
     ]
 
   },
