b50a23acca65cd11e034629d574e5a991d2fcb96
Erko Bridee <erko.bridee@gmail.com>
2017-04-21 15:46:03 -0300

    update angular combobox 02 example component code

--- src/lab/directives/angularjs/combobox/02/app/newer-selectone/component.js
+++ src/lab/directives/angularjs/combobox/02/app/newer-selectone/component.js
@@ -4,14 +4,16 @@
   // https://toddmotto.com/exploring-the-angular-1-5-component-method/
   // https://code.angularjs.org/1.6.4/docs/guide/component
 
+  var newerSelectOne = {
+    bindings : {
+      data : '<',
+      selected : '='
+    },
+    templateUrl : 'app/newer-selectone/template.html'
+  };
+
   angular
     .module( 'app' )
-    .component( 'newerSelectOne', {
-      bindings : {
-        data : '<',
-        selected : '='
-      },
-      templateUrl : 'app/newer-selectone/template.html'
-    });
+    .component( 'newerSelectOne', newerSelectOne );
 
 })();
